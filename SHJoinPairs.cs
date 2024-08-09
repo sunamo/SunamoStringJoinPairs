56077096fd1ef3bc74bc6b62b879a675906547cb
sunamo.cz <radek.jancik@sunamo.cz>
2024-08-09 14:40:31 +0200

    Trim XlfKeys

--- SHJoinPairs.cs
+++ SHJoinPairs.cs
@@ -12,12 +12,13 @@ public class SHJoinPairs
         //InitApp.TemplateLogger.NotEvenNumberOfElements(type, "JoinPairs", @"args", parts);
         //InitApp.TemplateLogger.AnyElementIsNull(type, "JoinPairs", @"args", parts);
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < parts.Length; i++)
+        var sb = new StringBuilder();
+        for (var i = 0; i < parts.Length; i++)
         {
             sb.Append(parts[i++] + firstDelimiter);
             sb.Append(parts[i] + secondDelimiter);
         }
+
         return sb.ToString();
     }
 }
