cad6d4c8b3c37db7d3e1cbecb08c00a4f16b57c7
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-16 14:22:00 +0200

    Before auto removing non public code elements

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -8,3 +8,4 @@ global using SunamoStringJoinPairs._sunamo.SunamoExceptions;
 global using System.Diagnostics;
 global using System.Reflection;
 global using System.Net;
+global using System.Diagnostics.CodeAnalysis;
